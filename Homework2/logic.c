87ab68c0a3319940848fe31ffef7418ea67cf796
MaxwellJFleming <71738689+MaxwellJFleming@users.noreply.github.com>
2021-10-15 17:41:37 -0500

    homework 2 update

--- Homework2/logic.c
+++ Homework2/logic.c
@@ -5,39 +5,59 @@
 
 //1
 
-//int reverse(const int n) {
+int reverse(const int n) {
   // Your job is to reverse an integer
   // sample input 123
   // sample output 321
-  //return 0;
-//}
+  int original_num = n;
+  int reverse_num = 0;
+
+  while (original_num > 0){
+    reverse_num = (reverse_num * 10) + (original_num % 10);
+    original_num = original_num / 10;
+  }
+  return reverse_num;
+}
 
 //2
-int i;
-
-int fib(int a, int b, int target){
+int fib(int a, int b, int target, int i){
   int next = a + b;
-  printf("%d\n", next);
+  //printf("%d\n", next);
   if (i == target){
-    return next;
-  } else {
-    i++;
-    fib(b, next, target);
+    return a;
   }
-  return i;
+  i++;
+  fib(b, next, target, i);
 }
 
 int fibbonacci(const int n) {
-  i = 2;
-  return fib(1, 1, n);
+  return fib(0, 1, n, 0);
 }
 
 //3
 
-//Result *pythagorean_triples(const int n) {
-  // calculate 3 integers a, b, c
-  // such that a ^ 2 + b ^ 2 + c^ 2 = n
-  // return the results as the provided struct
-  // or NULL if there is no set
-  //return NULL;
-//}
+Result *pythagorean_triples(const int n) {
+  Result *r;
+  r = malloc(sizeof(Result));
+  r->a = 1;
+  r->b = 1;
+  r->c = 1;
+  
+  while (r->a < r->b)
+  {
+    while (r->b < r->c)
+    {
+      while (r->c < n)
+      {
+        if ((r->a ^ 2) + (r->b ^ 2) + (r->c ^ 2) == n && (r->a ^ 2) + (r->b ^ 2) == (r->c ^ 2))
+        {
+          return r;
+        }
+        r->c++;
+      }
+      r->b++;
+    }
+    r->a++;
+  }
+  return NULL;
+}
--- Homework2/main.c
+++ Homework2/main.c
@@ -2,12 +2,36 @@
 #include <stdio.h>
 
 int main(void) {
-  int fib = fibbonacci(5);
-  printf("This should be 5: %d\n", fib);
+  //reverse tests
+  printf("Now testing integer reverses\n");
+  int reverse_int = 123;
+  printf("The reverse of %d is %d\n", reverse_int, reverse(reverse_int));
+
+  reverse_int = 56789;
+  printf("The reverse of %d is %d\n", reverse_int, reverse(reverse_int));
+
+  reverse_int = 40001;
+  printf("The reverse of %d is %d\n", reverse_int, reverse(reverse_int));
+
+  //fib tests
+  printf("Now testing fib\n");
+  
+  int fib = fibbonacci(0);
+  printf("This should be 0: %d\n", fib);
 
   int fibb = fibbonacci(10);
   printf("This should be 55: %d\n", fibb);
 
   int fibc = fibbonacci(8);
   printf("This should be 21: %d\n", fibc);
+
+  int fibd = fibbonacci(5);
+  printf("This should be 5: %d\n", fibd);
+
+  //pythagorean triplet test
+  printf("Now testing pythagorean\n");
+  
+  Result *test = pythagorean_triples(50);
+  printf("This should be 3, 4, 5: %d, %d, %d\n", test->a, test->b, test->c);
+  free(test);
 }
